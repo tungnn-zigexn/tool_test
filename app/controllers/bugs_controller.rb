bc2ad44271992030d9019b3c28ae4a200657bb96
tungnn-zigexn <tungnguyenngoc@zigexn.vn>
2026-01-05 10:31:01 +0700

    feat: Add full CRUD functionality for bugs, including new views and updated index/task pages.

--- app/controllers/bugs_controller.rb
+++ app/controllers/bugs_controller.rb
@@ -1,12 +1,45 @@
 class BugsController < ApplicationController
   skip_load_and_authorize_resource
-  before_action :set_project, only: [:index, :import_from_sheet]
-  before_action :set_task, only: [:index, :import_from_sheet]
+  before_action :set_project
+  before_action :set_task
+  before_action :set_bug, only: [:show, :edit, :update, :destroy]
 
   def index
     @bugs = @task.bugs.order(created_at: :desc)
     @bugs = @bugs.by_category(params[:category]) if params[:category].present?
     @bugs = @bugs.by_priority(params[:priority]) if params[:priority].present?
+  end
+
+  def show
+  end
+
+  def new
+    @bug = @task.bugs.build
+  end
+
+  def create
+    @bug = @task.bugs.build(bug_params)
+    if @bug.save
+      redirect_to project_task_bugs_path(@project, @task), notice: 'Bug đã được tạo thành công.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @bug.update(bug_params)
+      redirect_to project_task_bug_path(@project, @task, @bug), notice: 'Bug đã được cập nhật thành công.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bug.soft_delete
+    redirect_to project_task_bugs_path(@project, @task), notice: 'Bug đã được xóa thành công.'
   end
 
   def import_from_sheet
@@ -35,6 +68,14 @@ class BugsController < ApplicationController
 
   def set_task
     @task = @project.tasks.find(params[:task_id])
+  end
+
+  def set_bug
+    @bug = @task.bugs.find(params[:id])
+  end
+
+  def bug_params
+    params.require(:bug).permit(:title, :content, :application, :category, :priority, :status, :dev_id, :tester_id, :image_video_url, :notes)
   end
 
   def extract_spreadsheet_id(url)
